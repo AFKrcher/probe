1978c40c78f320faa0a92c71a866cbad01c0c26b
Craig Robinson <raggy1995@gmail.com>
2021-05-08 15:59:33 +1000

    Adding name search to api endpoints

--- src/server/routes.js
+++ src/server/routes.js
@@ -6,13 +6,27 @@ WebApp.connectHandlers.use('/api/satellite', (req, res, next) => {
     async function getSats() {
     
         res.setHeader('Content-Type', 'application/json');
-        // Try and fetch from the database, else return an error
-  
+
         try {
-            const sats = await SatelliteCollection.find().fetch();
-            res.writeHead(200);
-            res.end( JSON.stringify( sats));
+
+            // TODO: Parse params middelware ? 
+
+            satName = req.query.name;
+
+            if (satName !== null && satName !== '') {
+                const sats = await SatelliteCollection.find({"name":satName}).fetch();
+                // TODO: Find way of refactoring this, doesn't like the res and const outside of same block 
+                res.writeHead(200);
+                res.end( JSON.stringify( sats ));
+            }
+            else {
+                const sats = await SatelliteCollection.find().fetch();
+                res.writeHead(200);
+                res.end( JSON.stringify( sats ));
+            }
     
+            
+
         }
         catch(err) {
             error = {error:"Could not fetch sats"}
@@ -20,6 +34,7 @@ WebApp.connectHandlers.use('/api/satellite', (req, res, next) => {
             res.end( JSON.stringify( error ));
         }
     };
+  
   
     getSats();
     
