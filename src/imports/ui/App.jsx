e0e9005705e8e16fe74c4abdca9db06eb3f7dcef
n8ores <admin@nathanparrott.com>
2021-05-08 16:43:19 +1200

    Add React Table

--- src/imports/ui/App.jsx
+++ src/imports/ui/App.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
-import { Hello } from './Hello.jsx';
+import { Table } from './Table.jsx';
 import { Info } from './Info.jsx';
 import { Sats } from './Satellite';
 
 export const App = () => (
   <div>
     <h1>Welcome to Meteor!</h1>
-    <Hello/>
+    <Table/>
     <Info/>
     <Sats/>
   </div>
