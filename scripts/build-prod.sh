7f816cbcf783fec00a88b4c4e0b536eb70ec4b78
justinthelaw <justinwingchunglaw@gmail.com>
2021-10-26 15:11:03 -0700

    Dockerfile/compose 90%, Settings.jsx error fixed

--- scripts/build-prod.sh
+++ scripts/build-prod.sh
@@ -3,4 +3,4 @@
 cd ./build && rm -rf src.tar.gz && rm -rf bundle
 cd ../src && meteor npm install && meteor build ../build
 cd ../build && tar xzf src.tar.gz
-docker-compose up -d --env-file src/private/.env.dev
+cd .. && docker-compose --env-file src/private/.env.prod up
