4c1a68a1c4ecb824f7d096789f8c04f5effd41db
justinthelaw <justinwingchunglaw@gmail.com>
2021-10-27 15:04:40 -0700

    Fixed Verify Email page

--- src/imports/ui/Accounts/Verify.jsx
+++ src/imports/ui/Accounts/Verify.jsx
@@ -1,22 +1,56 @@
-import React from "react";
+import React, { useContext } from "react";
+import { Meteor } from "meteor/meteor";
 // Imports
 import { useLocation } from "react-router-dom";
 import { Accounts } from "meteor/accounts-base";
+import HelpersContext from "../Dialogs/HelpersContext.jsx";
 
-// @material-ui
-import { Grid } from "@material-ui/core";
+// Components
+import { Home } from "../Home.jsx";
+import AlertDialog from "../Dialogs/AlertDialog.jsx";
 
 export const Verify = () => {
   const location = useLocation();
 
+  const { setOpenAlert, alert, setAlert } = useContext(HelpersContext);
+
   const token = location.search.slice(7, location.search.length);
-  Accounts.verifyEmail(token, (res, err) => {
-    console.log(res, err);
+
+  const buttonClick = (
+    <span
+      onClick={() => {
+        window.location.href = "/";
+      }}
+    >
+      CLOSE
+    </span>
+  );
+
+  Accounts.verifyEmail(token, (err, res) => {
+    if (err || res) {
+      setAlert({
+        title: "Error Encountered",
+        text: err?.reason,
+        actions: null,
+        closeAction: buttonClick,
+      });
+      setOpenAlert(true);
+    } else {
+      setAlert({
+        title: "Success!",
+        text: `Welcome, ${
+          Meteor.user().username
+        }! Your email has been successfully verified. You can now use add, update, and delete data on the website as a user.`,
+        closeAction: buttonClick,
+      });
+      setOpenAlert(true);
+    }
   });
 
   return (
-    <Grid container justifyContent="center" alignItems="center">
-      Verified!
-    </Grid>
+    <React.Fragment>
+      <AlertDialog bodyAlert={alert} />
+      <Home />
+    </React.Fragment>
   );
 };
