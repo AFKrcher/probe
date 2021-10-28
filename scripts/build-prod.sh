1936fd6fc6173711bd694b269ff36934303461fa
justinthelaw <justinwingchunglaw@gmail.com>
2021-10-28 14:02:43 -0700

    Modified bach scripts for Docker builds

--- scripts/build-prod.sh
+++ scripts/build-prod.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
 
-#Remove any old bundles
+# Remove any old bundles
+echo "Removing any old meteor build bundles..."
 cd ./build && rm -rf src.tar.gz && rm -rf bundle
-#Build the project into a .tar
+# Build the project into a .tar
+echo "Installing node_modules and building application..."
 cd ../src && meteor npm install --production --allow-superuser  && meteor build ../build --allow-superuser
-#Extract the .tar into the bundle
+# Extract the .tar into the bundle
+echo "Extracting bundle from src.tar.gz..."
 cd ../build && tar xzf src.tar.gz
-#Install npm modules inside the bundle
+# Install npm modules inside the bundle
+echo "Installing node_modules inside bundle..."
 cd bundle/programs/server && meteor npm install --production --allow-superuser
-#We now have a fully ready to deploy program with all npm dependencies installed.
+# We now have a fully ready to deploy program with all npm dependencies installed
