54002882bf40313638a6e224e5314c9279fcc272
Nathan Parrott <nathan.parrott@saberastro.com>
2021-10-28 11:58:16 -0600

    Do meteor build inside docker linux
    Doing Meteor npm install on windows will install different node_modules than doing it on linux. We need to build inside linux to ensure the build is compatible to run on the linux distro.

--- scripts/build-prod.sh
+++ scripts/build-prod.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
+#Remove any old bundles
 cd ./build && rm -rf src.tar.gz && rm -rf bundle
+#Build the project into a .tar
 cd ../src && meteor npm install --production && meteor build ../build
+#Extract the .tar into the bundle 
 cd ../build && tar xzf src.tar.gz
-cd .. && docker-compose --env-file src/private/.env.prod up --build
+#Install npm modules inside the bundle 
+cd bundle/programs/server && meteor npm install --production
+#We now have a fully ready to deploy program with all npm dependencies installed. 
