8e08c3704f8d424a7610ff5746a4ef7fa459953b
justinthelaw <justinwingchunglaw@gmail.com>
2021-10-29 09:09:52 -0700

    Fixed roles functionality

--- src/imports/ui/Admin/Admin.jsx
+++ src/imports/ui/Admin/Admin.jsx
@@ -5,6 +5,9 @@ import { ApproveSatellites } from "./ApproveSatellites";
 import { ApproveSchemas } from "./ApproveSchemas";
 import { ErrorLog } from "./ErrorLog";
 
+// Imports
+import ProtectedFunctionality from "../Helpers/ProtectedFunctionality.jsx";
+
 // @material-ui
 import {
   AppBar,
@@ -64,7 +67,7 @@ export const Admin = () => {
               toggleTab(1);
             }}
           >
-            Users
+            Approve Satellites
           </Button>
           <Divider
             orientation="vertical"
@@ -81,23 +84,6 @@ export const Admin = () => {
               toggleTab(2);
             }}
           >
-            Approve Satellites
-          </Button>
-          <Divider
-            orientation="vertical"
-            className={classes.divider}
-            flexItem
-          />
-          <Button
-            size="medium"
-            style={{ marginLeft: 5, marginRight: 5 }}
-            className={
-              ("tabButton", toggleState === 3 ? "tabs active-tabs" : "tabs")
-            }
-            onClick={() => {
-              toggleTab(3);
-            }}
-          >
             Approve Schemas
           </Button>
           <Divider
@@ -105,46 +91,95 @@ export const Admin = () => {
             className={classes.divider}
             flexItem
           />
-          <Button
-            size="medium"
-            style={{ marginLeft: 5, marginRight: 5 }}
-            className={
-              ("tabButton", toggleState === 4 ? "tabs active-tabs" : "tabs")
-            }
-            onClick={() => {
-              toggleTab(4);
+
+          <ProtectedFunctionality
+            component={() => {
+              return (
+                <React.Fragment>
+                  <Button
+                    size="medium"
+                    style={{ marginLeft: 5, marginRight: 5 }}
+                    className={
+                      ("tabButton",
+                      toggleState === 3 ? "tabs active-tabs" : "tabs")
+                    }
+                    onClick={() => {
+                      toggleTab(3);
+                    }}
+                  >
+                    Error Log
+                  </Button>
+                  <Divider
+                    orientation="vertical"
+                    className={classes.divider}
+                    flexItem
+                  />
+                  <Button
+                    size="medium"
+                    style={{ marginLeft: 5, marginRight: 5 }}
+                    className={
+                      ("tabButton",
+                      toggleState === 4 ? "tabs active-tabs" : "tabs")
+                    }
+                    onClick={() => {
+                      toggleTab(4);
+                    }}
+                  >
+                    Users
+                  </Button>
+                </React.Fragment>
+              );
             }}
-          >
-            Error Log
-          </Button>
+            requiredRoles={["admin"]}
+            loginRequired={true}
+            skeleton={false}
+          />
         </Toolbar>
       </AppBar>
-      <div className={toggleState === 1 ? "tabs active-tabs" : "inactive-tabs"}>
-        <Paper elevation={3} className={classes.contentContainer}>
-          <Users />
-        </Paper>
-        <Typography variant="caption" className={classes.gridCaption}>
-          Click to interact with a cell, Double-click to view user data
-        </Typography>
-      </div>
       <Paper
         elevation={3}
-        className={toggleState === 2 ? "tabs active-tabs" : "inactive-tabs"}
+        className={toggleState === 1 ? "tabs active-tabs" : "inactive-tabs"}
       >
         <ApproveSatellites />
       </Paper>
       <Paper
         elevation={3}
-        className={toggleState === 3 ? "tabs active-tabs" : "inactive-tabs"}
+        className={toggleState === 2 ? "tabs active-tabs" : "inactive-tabs"}
       >
         <ApproveSchemas />
       </Paper>
-      <Paper
-        elevation={3}
-        className={toggleState === 4 ? "tabs active-tabs" : "inactive-tabs"}
-      >
-        <ErrorLog />
-      </Paper>
+
+      <ProtectedFunctionality
+        component={() => {
+          return (
+            <React.Fragment>
+              <Paper
+                elevation={3}
+                className={
+                  toggleState === 3 ? "tabs active-tabs" : "inactive-tabs"
+                }
+              >
+                <ErrorLog />
+              </Paper>
+              <div
+                className={
+                  toggleState === 4 ? "tabs active-tabs" : "inactive-tabs"
+                }
+              >
+                <Paper elevation={3} className={classes.contentContainer}>
+                  <Users />
+                </Paper>
+                <Typography variant="caption" className={classes.gridCaption}>
+                  Click to interact with a cell, Double-click to view user data
+                </Typography>
+              </div>
+            </React.Fragment>
+          );
+        }}
+        requiredRoles={["admin"]}
+        loginRequired={true}
+        skeleton={false}
+      />
     </React.Fragment>
   );
 };
