27e3190eab59992877c4a33875c9a644c3b50db8
justinthelaw <justinwingchunglaw@gmail.com>
2021-11-03 08:43:10 -0700

    Modified prints for bash scripts/server

--- scripts/build-prod.sh
+++ scripts/build-prod.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
 # Remove any old bundles
-echo "Removing any old meteor build bundles..."
+echo "=> Removing any old meteor build bundles..."
 cd ./build && rm -rf src.tar.gz && rm -rf bundle
 # Build the project into a .tar
-echo "Installing node_modules and building application..."
+echo "=> Installing node_modules and building application..."
 cd ../src && meteor npm install --production --allow-superuser  && meteor build ../build --allow-superuser
 # Extract the .tar into the bundle
-echo "Extracting bundle from src.tar.gz..."
+echo "=> Extracting bundle from src.tar.gz..."
 cd ../build && tar xzf src.tar.gz
 # Install npm modules inside the bundle
-echo "Installing node_modules inside bundle..."
+echo "=> Installing node_modules inside bundle..."
 cd bundle/programs/server && meteor npm install --production --allow-superuser
 # We now have a fully ready to deploy program with all npm dependencies installed
